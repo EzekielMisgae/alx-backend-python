c5092db93b62161799dcfb7378c2120ce484ed0d
ezekielalene <ezkielmisgae@gmail.com>
2023-02-27 20:42:43 +0300

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,3 +17,24 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org_name: str, mock_get_json: Dict):
         GithubOrgClient(org_name).org()
         mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+
+class TestGithubOrgClient(unittest.TestCase):
+    @patch('client.get_json')
+    def test_org(self, mock_get_json):
+        test_payload = {'test_key': 'test_value'}
+        mock_get_json.return_value = test_payload
+        client = GithubOrgClient('test_org')
+        self.assertEqual(client.org, test_payload)
+        mock_get_json.assert_called_once_with('https://api.github.com/orgs/test_org')
+
+    @patch.object(GithubOrgClient, 'org', new_callable=property)
+    def test_public_repos_url(self, mock_org):
+        test_payload = [{'name': 'test_repo1'}, {'name': 'test_repo2'}]
+        mock_org.return_value = {'repos_url': 'http://test-repos-url.com'}
+        with patch('client.get_json', return_value=test_payload) as mock_get_json:
+            client = GithubOrgClient('test_org')
+            self.assertEqual(client._public_repos_url, 'http://test-repos-url.com')
+            mock_get_json.assert_called_once_with('http://test-repos-url.com')
+
+if __name__ == '__main__':
+    unittest.main()
